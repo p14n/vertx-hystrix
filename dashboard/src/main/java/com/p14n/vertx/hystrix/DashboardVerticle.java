d9f02e19109bc2514d64991a9b2134c54f54479b
Dean Pehrsson-Chapman <dean@p14n.com>
2014-02-15 20:43:01 +0000

    Dashboard working

--- dashboard/src/main/java/com/p14n/vertx/hystrix/DashboardVerticle.java
+++ dashboard/src/main/java/com/p14n/vertx/hystrix/DashboardVerticle.java
@@ -1,19 +1,12 @@
 package com.p14n.vertx.hystrix;
 
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.HttpServerResponse;
-import org.vertx.java.core.http.impl.MimeMapping;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Dean Pehrsson-Chapman
@@ -43,66 +36,24 @@ public class DashboardVerticle extends Verticle {
         if ("/stream".equals(request.path())) {
           stream(request, delay);
         } else if ("/".equals(request.path())) {
-          file(request, "./index.html");
+          request.response().sendFile("./index.html");
         } else {
-          file(request, "." + request.path());
+          request.response().sendFile("." + request.path());
         }
       }
     }).listen(port);
 
   }
 
-  private byte[] getFile(String path) {
-    InputStream i = getClass().getClassLoader().getResourceAsStream(path);
-    try {
-      ByteArrayOutputStream o = new ByteArrayOutputStream();
-      while (i.available() > 0) {
-        o.write(i.read());
-      }
-      return o.toByteArray();
-    } catch (IOException e) {
-      return null;
-    }
-  }
-
-  private void file(HttpServerRequest request, String filename) {
-    byte[] file = getFile(filename);
-    if (file != null) {
-      HttpServerResponse response = request.response();
-      response.putHeader(org.vertx.java.core.http.HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length));
-      int li = filename.lastIndexOf('.');
-      if (li != -1 && li != filename.length() - 1) {
-        String ext = filename.substring(li + 1, filename.length());
-        String contentType = MimeMapping.getMimeTypeForExtension(ext);
-        if (contentType != null) {
-          response.putHeader(org.vertx.java.core.http.HttpHeaders.CONTENT_TYPE, contentType);
-        }
-      }
-      request.response().write(new Buffer(file));
-      request.response().end();
-      request.response().close();
-    } else {
-      sendError(request, 404, filename + " not found");
-    }
-  }
-
-  private void sendError(HttpServerRequest req, int error, String message) {
-    req.response().setStatusMessage(message);
-    req.response().setStatusCode(error);
-    req.response().end();
-  }
-
   private void stream(HttpServerRequest request, String delay) {
 
     final HttpServerResponse response = request.response();
 
+    response.setChunked(true);
+
     response.headers().add("Content-Type", "text/event-stream;charset=UTF-8");
     response.headers().add("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
     response.headers().add("Pragma", "no-cache");
-
-    response.setChunked(true);
-
-    final AtomicLong timerId = new AtomicLong(-1);
 
     vertx.eventBus().publish("hystrix-dashboard-admin", delay);
 
@@ -110,9 +61,11 @@ public class DashboardVerticle extends Verticle {
       @Override
       public void handle(Message<String> event) {
         try {
+
           String json = event.body();
-          response.write(json+"\n");
-        } catch (Exception e){
+          response.write("data: " + json + "\n\n");
+
+        } catch (Exception e) {
           e.printStackTrace();
           vertx.eventBus().publish("hystrix-dashboard-admin", "0");
         }
